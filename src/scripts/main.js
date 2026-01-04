b66d92958f1830f7f374f4eb8ab5f747eaa62d25
Nazar Kundys <nazarrisd@gmail.com>
2026-01-04 16:45:05 +0200

    ✨ feat: implement expandable tree functionality
    - Add click handler to toggle tree node visibility
    - Implement `WrapTextWithSpan()` to wrap text nodes in spans
    - Update demo link in README

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const ulTree = document.querySelector('.tree');
+
+WrapTextWithSpan();
+
+ulTree.addEventListener('click', (e) => {
+  if (e.target.tagName === 'SPAN') {
+    const parentSelected = e.target.closest('li');
+
+    const closestUL =
+      parentSelected.querySelector('ul') ?? parentSelected.parentElement;
+
+    closestUL.style.display =
+      closestUL.style.display === 'none' ? null : 'none';
+  }
+});
+
+function WrapTextWithSpan() {
+  ulTree.querySelectorAll('li').forEach((li) => {
+    li.childNodes.forEach((childNode) => {
+      if (childNode.nodeType === Node.TEXT_NODE) {
+        const span = document.createElement('span');
+
+        span.textContent = childNode.textContent;
+        childNode.replaceWith(span);
+      }
+    });
+  });
+}
